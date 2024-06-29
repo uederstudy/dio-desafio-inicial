e52b6d3442999123e367a5d7b4d0953f330f7fd0
uederstudy <uedersilva@live.com>
2024-06-29 00:17:54 -0300

    Create nivel-heroi-v0.1.js
    Adicionando primeiro código do desafio de lógica de programação da DIO

---
+++ nivel-heroi-v0.1.js
@@ -0,0 +1,26 @@
+// 1. Declarar Variáveis
+let nome = "Arthur";
+let xp = 7200;
+let nivel = "";
+
+// 2. Estrutura de Decisão para Determinar o Nível do Herói
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+} else if (xp >= 10001) {
+    nivel = "Radiante";
+}
+
+// 3. Exibir a Mensagem Final
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
